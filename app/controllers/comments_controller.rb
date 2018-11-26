912a5a2e69593c5a2e9b93d67e7a4a2a9e232b04
Gyutra <dimonbasist123@gmail.com>
2018-11-26 18:59:34 +0200

    2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  #http_basic_authenticate_with name: "admin", password: "123", 
+  http_basic_authenticate_with name: "amin", password: "123", 
 
 	def create
 		@post = Post.find(params[:post_id])
